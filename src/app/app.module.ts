405860a923f65d9ef0be4d50a3888abc32c0a2c2
Dawudesign <contact@dawudesign.fr>
2022-10-25 09:10:16 +0200

    finish

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BlockComponent } from './block/block.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BlockComponent
   ],
   imports: [
     BrowserModule,
